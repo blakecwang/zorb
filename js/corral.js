a7b620e45e3a60328747fbccbfdb5a55b50d8ead
Blake Wang <bwang@intouchhealth.com>
2018-11-16 07:40:04 -0800

    Add function for adding notepad shapes

--- js/corral.js
+++ js/corral.js
@@ -163,6 +163,23 @@ function stopRulerBlinking() {
   $(".tick").removeClass("white");
 }
 
+// Add a shape to the notepad.
+function addShapeToNotepad(shape, row, side) {
+  if (shape == 'green' || shape == 'blue') {
+    var klass = 'wide';
+  } else if (shape == 'pink' || shape == 'yellow') {
+    var klass = 'tall';
+  } else {
+    var klass = 'iso';
+  }
+  var selector = `#${row}-${side}`;
+  var img = `<img src="img/${shape}.png" class="${klass}" />`;
+  $(selector).prepend(img)
+}
+
 setupCorralCanvas();
 addBlinkingShapes();
 makeShapesClickable();
+
+addShapeToNotepad('red', 1, 'L');
+addShapeToNotepad('orange', 1, 'R');
