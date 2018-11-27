9ff1f2fca4f68b01df8dac28ade56502ec44094c
Blake Wang <bwang@intouchhealth.com>
2018-11-27 08:06:08 -0800

    Record video and audio!

--- js/reflection.js
+++ js/reflection.js
@@ -1,3 +1,8 @@
+var mediaRecorder,
+    chunks,
+    blob,
+    videoURL;
+
 // Detect whether the user's browser supports video.
 function hasGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
@@ -6,32 +11,64 @@ function hasGetUserMedia() {
 // Replace the canvas with a video element.
 function addVideo() {
   // Check browser compatability.
-  if (hasGetUserMedia()) {
-    console.log('Good to go!');
-  } else {
+  if (!hasGetUserMedia()) {
     alert("Your browser does not support video!");
   }
 
-  // Remove the canvas and add the video element.
+  // Restructure the DOM for reflections.
   $("#upper").remove();
   $("#middle").remove();
   $("#corral").remove();
   var videoWrapper = $("<div id='video-wrapper' class='col-xs-9' />");
   videoWrapper.prependTo($("#lower"));
-  var videoElem = $("<video autoplay />");
+  var videoElem = $("<video autoplay muted id='vid-record' />");
   videoElem.appendTo($("#video-wrapper"));
+  var recordElem = $("<div id='record' class='row'>RECORD</div>");
+  recordElem.appendTo($("#notepad"));
+  var stopElem = $("<div id='stop' class='row'>STOP</div>");
+  stopElem.appendTo($("#notepad"));
 
-  var vidWidth = $("#video-wrapper").width();
-  var vidHeight = $("#video-wrapper").height();
-  console.log(vidWidth);
-  console.log(vidHeight);
+  var playElem = $("<video id='vid-play' />");
+  playElem.appendTo($("#notepad"));
 
   // Set up the video stream.
   const constraints = { video: true, audio: true };
-  const video = document.querySelector("video");
-  navigator.mediaDevices.getUserMedia(constraints).
-    then((stream) => { video.srcObject = stream });
+  const video = document.querySelector("#vid-record");
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      video.srcObject = stream;
+      mediaRecorder = new MediaRecorder(stream);
+      console.log(mediaRecorder.state);
 
+      chunks = [];
+      mediaRecorder.ondataavailable = function(e) {
+        chunks.push(e.data);
+      }
+
+      mediaRecorder.onstop = function(e) {
+        console.log("onstop");
+        blob = new Blob(chunks);
+        videoURL = window.URL.createObjectURL(blob);
+        var playVid = document.querySelector("#vid-play");
+        playVid.src = videoURL;
+        playVid.play();
+      }
+    })
+    .catch(function(err) {
+      console.log('The following getUserMedia error occured: ' + err);
+    });
+
+  $("#record").click(function() {
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+  })
+
+  $("#stop").click(function() {
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+  })
 }
 
 // Init reflection time.
@@ -40,3 +77,9 @@ function reflectionTime() {
     addVideo();
   }, 1000);
 }
+reflectionTime();
+
+//var record = document.querySelector('#record');
+//var stop = document.querySelector('#stop');
+//var soundClips = document.querySelector('#sound-clips');
+
