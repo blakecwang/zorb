15fe5800533db00de1edd4c8691f06a0c9b53725
Blake Wang <bwang@intouchhealth.com>
2018-12-04 08:20:36 -0800

    Get camera button working

--- js/reflection.js
+++ js/reflection.js
@@ -3,34 +3,33 @@ function hasGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
-// Replace the game with a camera logo.
-function addCamera() {
-  // Restructure the DOM for reflections.
+// Restructure the page for video reflection.
+function addVideoWrapper() {
   $("#upper").remove();
   $("#middle").remove();
   $("#corral").remove();
   var videoWrapper = $("<div id='video-wrapper' class='col-xs-9' />");
   videoWrapper.prependTo($("#lower"));
+}
 
-  // Add controls.
-  var recordElem = $("<div id='record' class='row'>RECORD</div>");
-  recordElem.appendTo($("#notepad"));
-  var stopElem = $("<div id='stop' class='row'>STOP</div>");
-  stopElem.appendTo($("#notepad"));
-
-  // Make record button clickable.
-  $("#record").click(function(e) {
+// Replace the game with a camera logo.
+function addCamera() {
+  var cameraElem = $("<img id='camera' src='img/camera.png' />");
+  cameraElem.appendTo($("#video-wrapper"));
+  $("#camera").click(function(e) {
     addVideo();
   });
 }
 
 // Replace the canvas with a video element.
 function addVideo() {
-  console.log("addVideo() was called");
   // Check browser compatability.
   if (!hasGetUserMedia()) {
     alert("Your browser does not support video!");
   }
+
+  // Remove the camera.
+  $("#camera").remove();
 
   // Add the video element.
   var videoElem = $("<video autoplay muted id='vid-record' />");
@@ -48,8 +47,11 @@ function addVideo() {
 // Init reflection time.
 function reflectionTime() {
   setTimeout(function() {
-    addCamera();
+    addVideoWrapper();
   }, 1000);
+  setTimeout(function() {
+    addCamera();
+  }, 8500);
 }
 
 $("#main").addClass("hidden");
