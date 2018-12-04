0d3d9adca73781987b56d9f318a459842917168a
Blake Wang <bwang@intouchhealth.com>
2018-12-04 08:00:01 -0800

    Improve reflection a bit

--- js/reflection.js
+++ js/reflection.js
@@ -3,29 +3,38 @@ function hasGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
+// Replace the game with a camera logo.
+function addCamera() {
+  // Restructure the DOM for reflections.
+  $("#upper").remove();
+  $("#middle").remove();
+  $("#corral").remove();
+  var videoWrapper = $("<div id='video-wrapper' class='col-xs-9' />");
+  videoWrapper.prependTo($("#lower"));
+
+  // Add controls.
+  var recordElem = $("<div id='record' class='row'>RECORD</div>");
+  recordElem.appendTo($("#notepad"));
+  var stopElem = $("<div id='stop' class='row'>STOP</div>");
+  stopElem.appendTo($("#notepad"));
+
+  // Make record button clickable.
+  $("#record").click(function(e) {
+    addVideo();
+  });
+}
+
 // Replace the canvas with a video element.
 function addVideo() {
+  console.log("addVideo() was called");
   // Check browser compatability.
   if (!hasGetUserMedia()) {
     alert("Your browser does not support video!");
   }
 
-  // Remove stuff.
-  $("#upper").remove();
-  $("#middle").remove();
-  $("#corral").remove();
-
   // Add the video element.
-  var videoWrapper = $("<div id='video-wrapper' class='col-xs-9' />");
-  videoWrapper.prependTo($("#lower"));
   var videoElem = $("<video autoplay muted id='vid-record' />");
   videoElem.appendTo($("#video-wrapper"));
-
-  // Add the speaker element.
-  var speakerWrapper = $("<div id='speaker-wrapper' class='col-xs-3' />");
-  speakerWrapper.appendTo($("#lower"));
-  var speakerElem = $("<img source='img/speaker.png' id='speaker-btn' />");
-  speakerElem.appendTo($("#speaker-wrapper"));
 
   // Set up the video stream.
   const constraints = { video: true };
@@ -39,7 +48,8 @@ function addVideo() {
 // Init reflection time.
 function reflectionTime() {
   setTimeout(function() {
-    addVideo();
+    addCamera();
   }, 1000);
 }
+
 $("#main").addClass("hidden");
