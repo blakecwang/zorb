7d31bf84343a790de5c143f27f586b222eed97b4
Blake Wang <bwang@intouchhealth.com>
2018-12-04 18:50:34 -0800

    Style stop button

--- js/google.js
+++ js/google.js
@@ -5,7 +5,6 @@ function onSignIn(googleUser) {
 
   // Welcome the learner.
   playSound("hello", 0);
-reflectionTime();
 };
 
 function signOut() {
--- js/reflection.js
+++ js/reflection.js
@@ -37,8 +37,10 @@ function addVideo() {
   $("#camera").remove();
 
   // Add stop button.
-  var stopElem = $("<div id='stop' class='row'>STOP</div>");
-  stopElem.appendTo($("#notepad"));
+  var stopWrapper = $("<div id='stop-wrapper' class='row'></div>");
+  stopWrapper.appendTo($("#notepad"));
+  var stopElem = $("<div id='stop'>STOP</div>");
+  stopElem.appendTo($("#stop-wrapper"));
 
   // Add the video element.
   var videoElem = $("<video autoplay muted id='vid-record' />");
@@ -51,7 +53,6 @@ function addVideo() {
     .then(function(stream) {
       video.srcObject = stream;
       mediaRecorder = new MediaRecorder(stream);
-      console.log(mediaRecorder.state);
 
       chunks = [];
       mediaRecorder.ondataavailable = function(e) {
@@ -59,28 +60,26 @@ function addVideo() {
       }
 
       mediaRecorder.onstop = function(e) {
-        console.log("onstop");
+        // Stop the camera.
+        var tracks = stream.getTracks();
+        tracks.forEach(function(track) {
+          track.stop();
+        });
+
         blob = new Blob(chunks);
         videoURL = window.URL.createObjectURL(blob);
 
         $("#vid-record").remove();
-        var playElem = $("<video id='vid-play' />");
+        var playElem = $("<video controls id='vid-play' />");
         playElem.appendTo($("#video-wrapper"));
 
         var playVid = document.querySelector("#vid-play");
         playVid.src = videoURL;
         playVid.play();
-
-        var stopElem = $("<div id='play' class='row'>PLAY</div>");
-        stopElem.appendTo($("#notepad"));
-        $("#play").click(function() {
-          playVid.play();
-        });
       }
 
+      // Start recording.
       mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
     })
     .catch(function(err) {
       console.log('The following getUserMedia error occured: ' + err);
@@ -89,8 +88,7 @@ function addVideo() {
 
   $("#stop").click(function() {
     mediaRecorder.stop();
-    console.log(mediaRecorder.state);
-    console.log("recorder stopped");
+    $(this).remove();
   });
 }
 
